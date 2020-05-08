2516654a74775b249806b7caa0d00354db77520a
Keisuke Ueda <ueda.keisuke@me.com>
2020-05-07 22:08:08 -0700

    Updated

--- sample03/src/App.js
+++ sample03/src/App.js
@@ -1,36 +1,106 @@
 import React from 'react';
+import {BrowserRouter, Route, Link} from "react-router-dom";
+import {createStore} from 'redux'
 import './App.css';
 
-function App() {
-    let items = [
-        {"name" : "いちご", "price" : "100"},
-        {"name" : "りんご", "price" : "150"},
-        {"name" : "バナナ", "price" : "230"},
-    ]
+// function App() {
+//     return(
+//         <div className={"container text-center"}>
+//             <Clock />
+//         </div>
+//     )
+// }
 
-    return(
-        <div className="App container">
-            <table className="table table-striped">
-                <tbody>
-                {items.map((value) => (
-                    <tr>
-                        <th scope="row">{value.name}</th>
-                        <td>{value.price}円</td>
-                    </tr>
-                ))}
-                </tbody>
-            </table>
+const App = () => (
+    <BrowserRouter>
+        <div class="container text-center mt-5">
+            <Route exact path="/" component={Home}/>
+            <Route exact path="/about" component={About}/>
+            <Route exact path="/blog/:id" component={Blog}/>
+            <Route exact path="/sum/:num1/:num2" component={Sum}/>
+        </div>
+    </BrowserRouter>
+)
+//
+//
+// class Clock extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.now = new Date();
+//         this.state = {
+//             time: `${this.now.getHours()}:${this.now.getMinutes()}:${this.now.getSeconds()}`
+//         }
+//
+//         this.refresh = this.refresh.bind(this);
+//     }
+//
+//     refresh() {
+//         this.now = new Date();
+//         this.setState((state) => ({
+//             time: `${this.now.getHours()}:${this.now.getMinutes()}:${this.now.getSeconds()}`
+//         }));
+//     }
+//
+//     render() {
+//         return <p onClick={this.refresh}>{this.state.time}</p>
+//     }
+//
+// }
+
+
+const vote = (state=0, action) => {
+    switch(action.type){
+        case 'ADD':
+            return state + 1
+        default:
+            return state
+    }
+}
+
+let store = createStore(vote)
+
+const Home = () => {
+    store.dispatch({type: 'ADD'})
+    let x = store.getState().toString()
+    return (
+        <div>
+            <h1>Welcome</h1>
+            <p>投票数: {x}</p>
+            <p><a href="/about">aaa</a></p>
+            <p><Link to={"/about"}>about</Link></p>
         </div>
     )
 }
 
-class Clock extends React.Component
-{
-    render()
-    {
-        return <p>コンポーネント</p>
-    }
-
+const About = () => {
+    let x = store.getState().toString()
+    return (
+        <div>
+            <h1>About</h1>
+            <p>投票数: {x}</p>
+        </div>
+    )
 }
+
+const Blog = (props) => {
+    const {id} = props.match.params
+
+    return (
+        <div>
+            <h1>{id}番目の記事です</h1>
+        </div>
+    )
+}
+
+const Sum = (props) => {
+    const {num1, num2} = props.match.params
+
+    return (
+        <div>
+            <p>{num1} + {num2} = {parseInt(num1) + parseInt(num2)}</p>
+        </div>
+    )
+}
+
 
 export default App;
